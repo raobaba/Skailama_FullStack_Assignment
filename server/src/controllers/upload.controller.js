08b07c59b9d47ba4205dad20c928d52f3f721937
Rajan Kumar <raorajan9576@gmail.com>
2023-12-22 05:37:08 +0530

    completed update description

--- server/src/controllers/upload.controller.js
+++ server/src/controllers/upload.controller.js
@@ -44,8 +44,38 @@ const createDetails = async (req, res) => {
         .json({ message: "Error saving details.", error: err.message });
     }
   };
+
+  const updateDescription = async (req, res) => {
+    const { uploadId, fileId,detailsId } = req.params;
+    const { description } = req.body;
+  
+    try {
+      const upload = await Upload.findById(uploadId);
+      if (!upload) {
+        return res.status(404).json({ message: "Upload not found." });
+      }
+     console.log(upload)
+      const fileToUpdate = upload.files.id(fileId);
+      if (!fileToUpdate) {
+        return res.status(404).json({ message: "File not found." });
+      }
+    console.log(fileToUpdate)
+      const detailsToUpdate = fileToUpdate.details.id(detailsId);
+      if (!detailsToUpdate) {
+        return res.status(404).json({ message: "Details not found." });
+      }
+  console.log(detailsToUpdate)
+      detailsToUpdate.description = description;
+      await upload.save(); // Save the 'upload' object, not the Upload model itself
+      res.status(200).json({ upload });
+    } catch (err) {
+      res.status(500).json({ message: "Error updating description.", error: err.message });
+    }
+  };
+  
   
 module.exports = {
   createUpload,
   createDetails,
+  updateDescription
 };
--- server/src/routes/upload.route.js
+++ server/src/routes/upload.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const uploadRouter = express.Router();
-const { createUpload, createDetails } = require("../controllers/upload.controller.js");
+const { createUpload, createDetails,updateDescription } = require("../controllers/upload.controller.js");
 
 uploadRouter.post("/upload", createUpload); 
 uploadRouter.post('/details/:uploadId/:fileId',createDetails);
+uploadRouter.put('/update/:uploadId/:fileId/:detailsId',updateDescription)
 
 module.exports = uploadRouter;
