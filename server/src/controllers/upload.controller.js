56d0a40141a8cb2c7a943f2426e63abfd37a627f
Rajan Kumar <raorajan9576@gmail.com>
2023-12-22 05:47:45 +0530

    complete backend api creaetd

--- server/src/controllers/upload.controller.js
+++ server/src/controllers/upload.controller.js
@@ -5,7 +5,7 @@ const Upload = require("../models/upload.model.js");
 const { uploadItems } = require("../constants.js");
 
 const createUpload = asyncHandler(async (req, res) => {
-  const { projectName } = req.body;
+  const { projectName, episode } = req.body;
   const userId = req.cookies.userId;
 
   if (!userId) {
@@ -13,8 +13,15 @@ const createUpload = asyncHandler(async (req, res) => {
       .status(401)
       .json({ message: "Please login to perform this action." });
   }
+
+  const kolkataTime = new Date().toLocaleString("en-US", {
+    timeZone: "Asia/Kolkata",
+  });
+
   const newUpload = new Upload({
     projectName,
+    episode,
+    timeStamp: kolkataTime,
     files: uploadItems,
     userId,
   });
@@ -24,58 +31,59 @@ const createUpload = asyncHandler(async (req, res) => {
 });
 
 const createDetails = async (req, res) => {
-    const { uploadId, fileId } = req.params;
-    const { name, description } = req.body;
-    try {
-      const upload = await Upload.findById(uploadId);
-      if (!upload) {
-        return res.status(404).json({ message: "Upload not found." });
-      }
-      const fileToUpdate = upload.files.id(fileId);
-      if (!fileToUpdate) {
-        return res.status(404).json({ message: "File not found." });
-      }
-      fileToUpdate.details.push({ name, description });
-      await upload.save(); 
-      res.status(200).json({ upload }); 
-    } catch (err) {
-      res
-        .status(500)
-        .json({ message: "Error saving details.", error: err.message });
+  const { uploadId, fileId } = req.params;
+  const { name, description } = req.body;
+  try {
+    const upload = await Upload.findById(uploadId);
+    if (!upload) {
+      return res.status(404).json({ message: "Upload not found." });
     }
-  };
+    const fileToUpdate = upload.files.id(fileId);
+    if (!fileToUpdate) {
+      return res.status(404).json({ message: "File not found." });
+    }
+    fileToUpdate.details.push({ name, description });
+    await upload.save();
+    res.status(200).json({ upload });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error saving details.", error: err.message });
+  }
+};
 
-  const updateDescription = async (req, res) => {
-    const { uploadId, fileId,detailsId } = req.params;
-    const { description } = req.body;
-  
-    try {
-      const upload = await Upload.findById(uploadId);
-      if (!upload) {
-        return res.status(404).json({ message: "Upload not found." });
-      }
-     console.log(upload)
-      const fileToUpdate = upload.files.id(fileId);
-      if (!fileToUpdate) {
-        return res.status(404).json({ message: "File not found." });
-      }
-    console.log(fileToUpdate)
-      const detailsToUpdate = fileToUpdate.details.id(detailsId);
-      if (!detailsToUpdate) {
-        return res.status(404).json({ message: "Details not found." });
-      }
-  console.log(detailsToUpdate)
-      detailsToUpdate.description = description;
-      await upload.save(); // Save the 'upload' object, not the Upload model itself
-      res.status(200).json({ upload });
-    } catch (err) {
-      res.status(500).json({ message: "Error updating description.", error: err.message });
+const updateDescription = async (req, res) => {
+  const { uploadId, fileId, detailsId } = req.params;
+  const { description } = req.body;
+
+  try {
+    const upload = await Upload.findById(uploadId);
+    if (!upload) {
+      return res.status(404).json({ message: "Upload not found." });
     }
-  };
-  
-  
+    console.log(upload);
+    const fileToUpdate = upload.files.id(fileId);
+    if (!fileToUpdate) {
+      return res.status(404).json({ message: "File not found." });
+    }
+    console.log(fileToUpdate);
+    const detailsToUpdate = fileToUpdate.details.id(detailsId);
+    if (!detailsToUpdate) {
+      return res.status(404).json({ message: "Details not found." });
+    }
+    console.log(detailsToUpdate);
+    detailsToUpdate.description = description;
+    await upload.save(); // Save the 'upload' object, not the Upload model itself
+    res.status(200).json({ upload });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error updating description.", error: err.message });
+  }
+};
+
 module.exports = {
   createUpload,
   createDetails,
-  updateDescription
+  updateDescription,
 };
